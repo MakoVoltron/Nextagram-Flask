4936e1fa552a3ffe90c133cb20c55fe30a3d89cc
Mate Valtr <matej.valtr@gmail.com>
2019-05-14 11:25:56 +0800

    Before push to Github

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+from models.users import User
+from flask_jwt jwt_required
 
 users_api_blueprint = Blueprint('users_api',
                              __name__,
@@ -7,3 +9,29 @@ users_api_blueprint = Blueprint('users_api',
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
     return "USERS API"
+
+@users_api_blueprint.route('/<username>', methods=['GET'])
+def show(username):
+    user = User.get_or_none(User.username == username)
+
+    if not user:
+        resp = {
+            'message': 'No user with this name was found',
+            'ok': False
+        }
+
+        return jsonify(resp)
+
+    resp = {
+        'message': 'Found the user with this username',
+        'ok': True,
+        'user': {
+            'is': user.id,
+            'username': user.username,
+            'email': user.email
+        },
+        'ok': True
+    }
+
+    return jsonify(resp)
+            
